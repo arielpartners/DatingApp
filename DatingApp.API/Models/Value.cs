cb40cd1ede5110a28b258358685d911f9765a3fd
Craeg Strong <cstrong@arielpartners.com>
2018-02-26 02:16:42 -0500

    initial checkin

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Value()
+        {
+        }
+    }
+}
